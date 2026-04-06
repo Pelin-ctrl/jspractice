05bfdc7904913f24cc4185060bfdd5c47856d9dc
Pelin <pelinakdulun2020@gmail.com>
2026-04-07 01:24:23 +0300

    update

--- practice.js
+++ practice.js
@@ -37,26 +37,72 @@
 
 
 
-// test3
+// // test3
+// let result;
+// let grid = [
+//     [0,1,0,1,0,0],
+//     [0,1,0,1,0,0],
+//     [1,1,0,1,1,0]
+//   ]
+// let bottomRow = grid[grid.length-1]
+// let chunks = 0;
+
+
+// // -- ваш код начинается тут
+
+// for (let i=0; i<bottomRow.length; i++){
+//     if (bottomRow[i]===0){
+//         chunks++
+//     }
+// }
+
+
+// result=chunks;
+
+// console.log(result)
+
+
+
+//test4
+
+// let result;
+// let data = [4, 3, 7, 1, 8, 6, 5, 2];
+// let even = [];
+// let uneven = [];
+
+
+// // -- ваш код начинается тут
+
+// for (let i=0; i<data.length; i++){
+//     if (data[i]%2===0){
+//         even.push(data[i]);
+//     }
+//     if (data[i]%2!==0){
+//         uneven.push(data[i]);
+//     }
+// }
+
+// even.sort((a, b) => a-b);
+// uneven.sort((a, b) => a-b);
+
+
+
+// result = `(${even.join(', ')}) (${uneven.join(', ')})`;
+
+// console.log(result)
+
+
+
 let result;
-let grid = [
-    [0,1,0,1,0,0],
-    [0,1,0,1,0,0],
-    [1,1,0,1,1,0]
-  ]
-let bottomRow = grid[grid.length-1]
-let chunks = 0;
 
-
-// -- ваш код начинается тут
-
-for (let i=0; i<bottomRow.length; i++){
-    if (bottomRow[i]===0){
-        chunks++
+for (let i = 0; i<items.length; i++){
+    if (items[i]==='Энергетик'){
+        result = power+=5
     }
-}
-
-
-result=chunks;
-
-console.log(result)
+    if (items[i]==='Кофе'){
+        result = power+=10
+    }
+    if (result>100){
+        result=100
+    }
+  }
